784bf7a4c8b20e3fc3e32f909067b7073c1ebc66
Santiago Sanguinetti <sanguinettisantiago@gmail.com>
2023-04-22 23:25:12 -0300

    PreEntrega2Sanguinetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemCount from "./components/ItemCount/ItemCount";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+
 function App() {
     return (
         <div>
-            <NavBar />
-            <ItemListContainer greeting={"Bienvenidos"} />
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<ItemListContainer />} />
+                    <Route
+                        path="/category/:categoryId"
+                        element={<ItemListContainer />}
+                    />
+                    <Route
+                        path="/item/:itemId"
+                        element={<ItemDetailContainer />}
+                    />
+                    <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,40 @@
 import "./NavBarStyles.css";
 import CartWidget from "./CartWidget/CartWidget";
+import { NavLink, Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <header>
             <nav className="navbar">
-                <h3>Horizon</h3>
-                <li className="navbar-menu">
-                    <a href="/#">Inicio</a>
-                </li>
-                <li className="navbar-menu">
-                    <a href="/#">Acerca de</a>
-                </li>
-                <li className="navbar-menu">
-                    <a href="/#">Contacto</a>
-                </li>
+                <Link to="/" className="Logo">
+                    <h3>Horizon</h3>
+                </Link>
+
+                <div className="Categories">
+                    <NavLink
+                        to={`/category/gpu`}
+                        className={({ isActive }) =>
+                            isActive ? "ActiveOption" : "Option"
+                        }
+                    >
+                        Tarjetas de video
+                    </NavLink>
+                    <NavLink
+                        to={`/category/cpu`}
+                        className={({ isActive }) =>
+                            isActive ? "ActiveOption" : "Option"
+                        }
+                    >
+                        Procesadores
+                    </NavLink>
+                    <NavLink
+                        to={`/category/motherboard`}
+                        className={({ isActive }) =>
+                            isActive ? "ActiveOption" : "Option"
+                        }
+                    >
+                        Placas madre
+                    </NavLink>
+                </div>
                 <CartWidget />
             </nav>
         </header>
